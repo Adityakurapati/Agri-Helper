7bc8fdcfce151ae0058a315a3d9eb5ae0253e7ac
HeyProject7 <124274247+HeyProject7@users.noreply.github.com>
2024-08-01 07:42:53 +0530

    running

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,23 +1,23 @@
-import PostCard from '../postCard/PostCard'  // Adjust this import based on how PostCard is exported
+import PostCard from '../../postCard/PostCard'
 import styles from './blog.module.css'
 import { getPosts } from '../../lib/data.js'
 
-
 const getData=async () =>
 {
-        const posts=await fetch( 'http:localhost:3000/api/blog' )
+        const posts=await fetch( 'http://localhost:3000/api/blog' )
         if ( !posts.ok )
         {
-                throw new Error( "Unable o fetch Posts" )
+                throw new Error( "Unable to fetch Posts" )
         }
         return posts.json();
 }
+
 const BlogPage=async () =>
 {
         try
         {
                 const posts=await getData();
-                // const posts=await getPosts();
+                // const posts = await getPosts();
                 return (
                         <section className={ styles.container }>
                                 { posts.map( ( post ) => (
