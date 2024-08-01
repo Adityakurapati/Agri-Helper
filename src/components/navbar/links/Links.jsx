1a84dc641194c465d276ab24796a07328a5dba76
HeyProject7 <124274247+HeyProject7@users.noreply.github.com>
2024-08-01 08:11:25 +0530

    added-import

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -11,7 +11,7 @@ const Links=( { session } ) =>
                 { title: "Home", path: "/" },
                 { title: "About", path: "/about" },
                 { title: "Contact", path: "/contact" },
-                { title: "Blog", path: "/blog" }
+                { title: "remote", path: "/remote" }
         ];
 
         const [ open, setOpen ]=useState( false );
